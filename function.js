b51c1f6ef908cd439d8d3a49804a99f714d90a98
saminnam <innam8264@gmail.com>
2024-09-22 14:36:42 +0530

    first commit

---
+++ function.js
@@ -0,0 +1,17 @@
+const msg = document.querySelector("#notification");
+
+function notificaion() {
+    return(
+        <>
+        <div class="msg-box">
+            <div class="box-1">
+                <img src=""/>
+            </div>
+            <div class="box-2">
+
+            </div>
+        </div>
+        </>
+    )
+    
+}
